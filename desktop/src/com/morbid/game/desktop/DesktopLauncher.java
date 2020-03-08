934dc9cd19176e6316a64f53dd23c95bb030ac0f
Tomasz Nieżurawski <tomek.niezurawski@gmail.com>
2020-03-08 13:21:53 +0100

    Updated .gitignore

---
+++ desktop/src/com/morbid/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.morbid.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.morbid.game.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
